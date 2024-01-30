6a04fc3e6dfbeac8c410c66186ed38b43b12abf8
Emirfahimi0 <emirfahimi0@gmail.com>
2024-01-30 12:06:56 +0800

    update types

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,6 +1,9 @@
-interface jobList {
+type TJobTypes = "Full-Time" | "Part-Time" | "FreeLance" | "Contract" | "Internship";
+
+interface JobInformation {
   jobTitle: string;
   companyTitle: string;
   salaryRange: string;
   srcPath: string;
+  jobTypes: TJobTypes;
 }
